75a9d91f5d432b3d8da599474fc92b3c5e80a6aa
stepin105092 <44312298+mariateres@users.noreply.github.com>
2020-10-10 00:49:57 +0530

    Update testBBMS.c

--- 3_Implementation/test/testBBMS.c
+++ 3_Implementation/test/testBBMS.c
@@ -51,8 +51,8 @@ int main() {
 void test_menu(void) {
   CU_ASSERT(1 == valid_choice(2));
   CU_ASSERT(1 == valid_choice(5));
-  CU_ASSERT(0 == valid_choice(8));
-  CU_ASSERT(0 == valid_choice(9));
+  CU_ASSERT(1 == valid_choice(3));
+  CU_ASSERT(1 == valid_choice(1));
 }
   /* Dummy fail*/
   //CU_ASSERT(2 == AddDonor());
