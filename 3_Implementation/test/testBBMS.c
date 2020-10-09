bb364449bec205a1a4baa55f252df43a4b776d88
stepin105092 <44312298+mariateres@users.noreply.github.com>
2020-10-10 00:33:39 +0530

    Update testBBMS.c

--- 3_Implementation/test/testBBMS.c
+++ 3_Implementation/test/testBBMS.c
@@ -6,14 +6,14 @@
 #define PROJECT_NAME    "BloodBankManagementSystem"
 
 /* Prototypes for all the test functions */
-
+/*
 void test_AddDonor(void);
 void test_List(void);
 void test_DeleteDonor(void);
 void test_Update(void);
 void test_Search(void);
-
-
+*/
+void test_menu(void);
 
 /* Start of the application test */
 int main() {
@@ -28,11 +28,12 @@ int main() {
   /* Add your test functions in this format for testing*/
 
 
-  CU_add_test(suite, "Added", test_AddDonor);
-  CU_add_test(suite, "List", test_List);
-  CU_add_test(suite, "Delete", test_DeleteDonor);
-  CU_add_test(suite, "Update", test_Update);
-  CU_add_test(suite, "Search", test_Search);
+  //CU_add_test(suite, "Added", test_AddDonor);
+  //CU_add_test(suite, "List", test_List);
+  //CU_add_test(suite, "Delete", test_DeleteDonor);
+  //CU_add_test(suite, "Update", test_Update);
+  //CU_add_test(suite, "Search", test_Search);
+	CU_add_test(suite, "Valid", test_menu);
 
 
 /* Note: Do not edit START*/
@@ -47,42 +48,52 @@ int main() {
 /* Note: Do not edit END */
   return 0;
 }
-
+void test_menu(void) {
+  CU_ASSERT(1 == valid_choice(2));
+  CU_ASSERT(1 == valid_choice(5));
+  CU_ASSERT(0 == valid_choice(8));
+  CU_ASSERT(0 == valid_choice(9));
+}
+  /* Dummy fail*/
+  //CU_ASSERT(2 == AddDonor());
+}*/
+/*
 /* Write all the test functions */
+/*
 void test_AddDonor(void) {
 
   CU_ASSERT(0 == AddDonor() || -1 == AddDonor());
 
   /* Dummy fail*/
   //CU_ASSERT(2 == AddDonor());
-}
-
+}*/
+/*
 void test_Update(void) {
   CU_ASSERT(0 == Update() || 1 == Update());
 
   /* Dummy fail*/
  // CU_ASSERT(-1 == Update());
-}
+}*/
 
-
+/*
 void test_DeleteDonor(void)
 {
 	CU_ASSERT(0 == DeleteDonor() || 1 == DeleteDonor() || -1 == DeleteDonor());
 
   /* Dummy fail*/
  // CU_ASSERT(2 == DeleteDonor());
-}
-
+}*/
+/*
 void test_List(void){
 	CU_ASSERT(0 == List() || 1 == List());
 
   /* Dummy fail*/
   //CU_ASSERT(-1 == List());
-}
-
+}*/
+/*
 void test_Search(void){
 	CU_ASSERT(0 == Search() || 1 == Search());
 
-  /* Dummy fail*/
+  /* Dummy fail
   //CU_ASSERT(-1 == Search());
-}
+}*/
